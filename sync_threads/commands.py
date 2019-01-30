d2fa6705485fb87f431b783e2113c7ad7a411700
Bohdan Kaminskyi <b.kaminskyi99@gmail.com>
2019-01-30 23:36:25 +0200

    add command registrator decorator

--- sync_threads/commands.py
+++ sync_threads/commands.py
@@ -1,11 +1,23 @@
 import os
 
+commands = {}
 
+
+def register_command(command_func):
+    commands[command_func.__name__] = command_func
+
+    def wrapper(*args, **kwargs):
+        return command_func(*args, **kwargs)
+    return wrapper
+
+
+@register_command
 def ls():
     """List directory contents"""
     return os.listdir(os.getcwd())
 
 
+@register_command
 def cd(args):
     """Change the shell working directory
 
@@ -17,6 +29,7 @@ def cd(args):
     os.chdir(path)
 
 
+@register_command
 def pwd():
     """Print working directory
 
