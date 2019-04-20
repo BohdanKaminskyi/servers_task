caa763d0b81054d5912e07b53182233ed5acb6b1
Bohdan Kaminskyi <b.kaminskyi99@gmail.com>
2019-04-21 00:14:00 +0300

    fix display of history indexes

--- sync_threads/commands.py
+++ sync_threads/commands.py
@@ -30,7 +30,7 @@ class HistoryViewer:
     def as_strings(self):
         return '\n'.join(
             map(
-                lambda command: '   ' + command,
+                lambda ind_command: f'{ind_command[0]:>5}  {ind_command[1]}',
                 enumerate(self._history)
                 )
             )
